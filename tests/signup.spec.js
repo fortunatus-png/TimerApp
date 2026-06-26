4199ee4860a4c6f306439cf3bdb9844f0ab3ae72
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-06-26 10:31:23 +0200

    test: add signup E2E tests

--- tests/signup.spec.js
+++ tests/signup.spec.js
@@ -1,19 +1,51 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Signup', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/login');
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('Successful signup with valid credentials', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('ye@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('stringst');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page).toHaveURL('/');
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test('Failed signup with invalid email format', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('ye@example');
+    await page.getByRole('textbox', { name: 'Password' }).fill('stringst');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page.getByText('Enter a valid email (e.g., name@domain.com)')).toBeVisible();
+  });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test('Failed signup with an empty email field', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('');
+    await page.getByRole('textbox', { name: 'Password' }).fill('stringst');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page.getByText('Email is required')).toBeVisible();
+  });
+
+  test('Failed signup with email that already exists', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('user@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('stringst');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page.getByText('Email already exists')).toBeVisible();
+  });
+
+  test('Failed signup with password too short', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('yeye@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('string');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page.getByText('Password must be at least 8 characters')).toBeVisible();
+  });
+
+  test('Failed signup with an empty password field', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('yeye@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page.getByText('Password is required')).toBeVisible();
+  });
 });
