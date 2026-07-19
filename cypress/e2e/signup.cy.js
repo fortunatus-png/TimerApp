48eb6584c2d6fc1b54064b8f082b3fe96c1762d0
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-07-19 12:39:15 +0200

    test(cypress): add login and home page E2E tests

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -2,42 +2,46 @@
 
 describe("Signup", () => {
     beforeEach("Visit login page", () => {
-        cy.visit("http://localhost:5173/login");
+        cy.visit("/login");
     });
 
     it("Successful signup with valid credentials", () => {
-        cy.get('[type="email"]').type("admin@example.com");
+        cy.get('[type="email"]').type("user1@example.com");
         cy.get('[type="password"]').type("stringst");
         cy.get('[type="button"]').contains("Sign Up").click();
+        cy.url().should('include', '/')
     });
 
     it("Failed signup with invalid email format", () => {
         cy.get('[type="email"]').type("adminexample.com");
         cy.get('[type="password"]').type("stringst");
         cy.get('[type="button"]').contains("Sign Up").click();
+        cy.contains('Enter a valid email (e.g., name@domain.com)').should('be.visible');
     });
 
     it("Failed signup with an empty email field", () => {
-        cy.get('[type="email"]').type("");
         cy.get('[type="password"]').type("stringst");
         cy.get('[type="button"]').contains("Sign Up").click();
+        cy.contains('Email is required').should('be.visible');
     });
 
     it("Failed signup with email that already exists", () => {
         cy.get('[type="email"]').type("admin@example.com");
         cy.get('[type="password"]').type("stringst");
         cy.get('[type="button"]').contains("Sign Up").click();
+        cy.contains('Email already exists').should('be.visible');
     });
 
     it("Failed signup with password too short", () => {
         cy.get('[type="email"]').type("host@example.com");
         cy.get('[type="password"]').type("strings");
         cy.get('[type="button"]').contains("Sign Up").click();
+        cy.contains('Password must be at least 8 characters').should('be.visible');
     });
 
-    it.only("Failed signup with an empty password field", () => {
+    it("Failed signup with an empty password field", () => {
         cy.get('[type="email"]').type("host@example.com");
-        cy.get('[type="password"]').type("");
         cy.get('[type="button"]').contains("Sign Up").click();
+        cy.contains('Password is required').should('be.visible');
     });
 })
