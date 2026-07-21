052eb22e6afa51b8501a6664489a0391c750f3a4
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-07-21 18:16:34 +0200

    test(cypress): add SessionPage POM and refactor tests

--- cypress/e2e/session.cy.js
+++ cypress/e2e/session.cy.js
@@ -1,64 +1,57 @@
 /// <reference types="cypress" />
+import SessionPage from '../support/pageObjects/SessionPage'
 
 describe("Session", () => {
+    const sessionPage = new SessionPage();
     beforeEach("Visit session page", () => {
-        cy.visit("/login");
-        cy.get('[type="email"]').type("user@example.com");
-        cy.get('[type="password"]').type("stringst");
-        cy.contains('button', 'Log In').click();
-        cy.contains('button', 'Timer').click();
-        cy.contains('button', 'Start').click();
-        cy.location('pathname').should('eq', '/session');
+        sessionPage.visitLoginPage();
+        sessionPage.login('user@example.com', 'stringst');
+        sessionPage.assertSessionSuccessful();
     });
 
     it("Sessionpage loads correctly", () => {
-        cy.get('h1').contains('5');
-        cy.get('.figure-svg').should('be.visible');
+        sessionPage.getSessionPageElements();
     });
 
     it("Pause the countdown timer", () => {
-        cy.contains('button', '⏸').click();
-        cy.contains('button', '▶').should('be.visible');
+        sessionPage.getPauseButton().click();
+        sessionPage.getPlayButton().should('be.visible');
     });
 
     it("Resume the countdown timer", () => {
-        cy.contains('button', '⏸').click();
-        cy.contains('button', '▶').click();
-        cy.contains('button', '⏸').should('be.visible');
+        sessionPage.getPauseButton().click();
+        sessionPage.getPlayButton().click();
+        sessionPage.getPauseButton().should('be.visible');
     });
 
     it("Warning message when leaving the page during active session", () => {
-        cy.contains('button', 'History').click();
-        cy.contains('Your progress so far will be saved, but you won\'t be able to continue this session later.').should('be.visible');
-        cy.contains('button', 'Continue').should('be.visible');
-        cy.contains('Leave').should('be.visible');
+        sessionPage.getHistoryButton().click();
+        sessionPage.getWarningMessage().should('be.visible');
+        sessionPage.getContinueButton().should('be.visible');
+        sessionPage.getLeaveButton().should('be.visible');
     });
 
     it("Continue session after warning", () => {
-        cy.contains('button', 'History').click();
-        cy.contains('button', 'Continue').click();
-        cy.contains('Your progress so far will be saved, but you won\'t be able to continue this session later.').should('not.exist');
-        cy.contains('button', '⏸').should('be.visible');
+        sessionPage.getHistoryButton().click();
+        sessionPage.getContinueButton().click();
+        sessionPage.getWarningMessage().should('not.exist');
+        sessionPage.getPauseButton().should('be.visible');
     });
 
     it("Leave session after warning", () => {
-        cy.contains('button', 'History').click();
-        cy.contains('button', 'Leave').click();
-        cy.location('pathname').should('eq', '/history');
+        sessionPage.getHistoryButton().click();
+        sessionPage.getLeaveButton().click();
+        sessionPage.assertHistoryPageSuccessful();
     });
 
     // it("Session completes successfully", () => {
     //     cy.wait(310000);
-    //     cy.contains("🎉 Great job!").should("be.visible");
-    //     cy.contains("You studied for 5 minutes!").should("be.visible");
-    //     cy.contains("button", "Start new session").should("be.visible");
+    //     sessionPage.assertSessionCompleteMessage();
     // });
 
     // it("Start new session after completion", () => {
-    //     cy.wait(320000);
-    //     cy.contains("🎉 Great job!").should("be.visible");
-    //     cy.contains("You studied for 5 minutes!").should("be.visible");
-    //     cy.contains("button", "Start new session").click();
-    //     cy.location('pathname').should('eq', '/timer');
+    //     cy.wait(310000);
+    //     sessionPage.assertSessionCompleteMessage();
+    //     sessionPage.assertTimerPageSuccessful();
     // });
 })
