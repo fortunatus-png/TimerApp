4609bf2ee80e4200283a57d5d1cbccd78879968c
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-07-25 16:43:21 +0200

    refactor(playwright): create timer POM

--- tests/pageObjects/TimerPage.js
+++ tests/pageObjects/TimerPage.js
@@ -1,84 +1,51 @@
-class TimerPage {
-    visitLoginPage() {
-        cy.visit('/login');
+import { expect } from '@playwright/test';
+
+export class TimerPage {
+    constructor(page) {
+        this.page = page;
+        this.emailField = page.getByRole('textbox', { name: 'Email' });
+        this.passwordField = page.getByRole('textbox', { name: 'Password' });
+        this.loginBtn = page.getByRole('button', { name: 'Log in' });
+
+        this.timerPageBtn = page.getByRole('button', { name: 'Timer' });
+        this.slider = page.getByRole('slider');
+        this.startButton = page.getByRole('button', { name: 'Start' });
+        this.panda = page.locator('.figure-svg');
+        this.studyPanda = page.getByRole('img');
+        this.initialTime = page.getByText('5 Minutes');
+        this.minutes25 = page.getByText('25 Minutes');
+        this.minutes180 = page.getByRole('heading', { name: '180' });
+        this.initialTimerHeader = page.getByRole('heading', { name: '5' });
     }
 
-    getEmailInput() {
-        return cy.get('[type="email"]');
+    async logIn(email, password) {
+        await this.emailField.fill(email);
+        await this.passwordField.fill(password);
+        await this.loginBtn.click();
     }
 
-    getPasswordInput() {
-        return cy.get('[type="password"]');
+    async visitLoginPage() {
+        await this.page.goto('/login');
     }
 
-    getLoginButton() {
-        return cy.contains('button', 'Log In');
+    async assertTimerPageSuccessful() {
+        await this.timerPageBtn.click();
+        await expect(this.page).toHaveURL('/timer');
     }
 
-    getTimerButton() {
-        return cy.contains('button', 'Timer');
+    async assertSessionPageSuccessful() {
+        await expect(this.page).toHaveURL('/session');
     }
 
-    getStartButton() {
-        return cy.contains('button', 'Start');
+    async assertTimerPageLoaded() {
+        await expect(this.slider).toBeVisible();
+        await expect(this.startButton).toBeVisible();
+        await expect(this.panda).toBeVisible();
+        await expect(this.initialTime).toBeVisible();
     }
 
-    getFiveMinutes() {
-        return cy.contains('5 Minutes');
-    }
-
-    getSlider() {
-        return cy.get('#slider');
-    }
-
-    getHomeMessage() {
-        return cy.get('.homeMessage');
-    }
-
-    getSVGFigure() {
-        return cy.get('.figure-svg');
-    }
-
-    getHeader() {
-        return cy.get('h1');
-    }
-
-    login(email, password) {
-        this.getEmailInput().type(email);
-        this.getPasswordInput().type(password);
-        this.getLoginButton().click();
-    }
-
-    assertLoginSuccessful() {
-        cy.location('pathname').should('eq', '/');
-    }
-
-    assertSessionSuccessful() {
-        this.getSVGFigure().should('be.visible');
-        cy.location('pathname').should('eq', '/session');
-    }
-
-    assertTimerPageSuccessful() {
-        this.getTimerButton().click();
-        cy.location('pathname').should('eq', '/timer');
-    }
-
-    getTimerPageElements() {
-        this.getSlider().should('be.visible');
-        this.getStartButton().should('be.visible');
-        this.getSVGFigure().should('be.visible');
-        this.getFiveMinutes().should('be.visible');
-    }
-
-    setupTime() {
-        this.getSlider().invoke("val", 25).trigger("change");
-        this.getSlider().should('have.value', '25');
-    }
-
-    setupMaxTime() {
-        this.getSlider().invoke("val", 180).trigger("change");
-        this.getSlider().should('have.value', '180');
+    async assertSessionPageLoaded() {
+        await expect(this.initialTimerHeader).toBeVisible();
+        await expect(this.studyPanda).toBeVisible();
     }
 }
-
-export default TimerPage;
--- tests/timer.spec.js
+++ tests/timer.spec.js
@@ -1,47 +1,45 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-import { StudyPandaPage } from './studyPandaPage';
+import { TimerPage } from './pageObjects/TimerPage';
 
 test.describe('Timer', () => {
   const validEmail = 'ye@example.com';
   const validPassword = 'stringst';
-  /** @type {StudyPandaPage} */
-  let logTimerPage;
+  /** @type {TimerPage} */
+  let timerPage;
 
   test.beforeEach(async ({ page }) => {
-    logTimerPage = new StudyPandaPage(page);
-    await logTimerPage.gotoLoginPage();
-    await logTimerPage.logIn(validEmail, validPassword);
-    await logTimerPage.timerPageBtn.click();
-    await logTimerPage.expectTimerPage();
+    timerPage = new TimerPage(page);
+    await timerPage.visitLoginPage();
+    await timerPage.logIn(validEmail, validPassword);
+    await timerPage.assertTimerPageSuccessful();
   });
 
-  test('Timerpage loads correctly', async ({ page }) => {
-    await logTimerPage.getTimerElements();
+  test('Timerpage loads correctly', async () => {
+    await timerPage.assertTimerPageLoaded();
   });
 
-  test('Set up the time with range slider', async ({ page }) => {
-    await logTimerPage.slider.fill('25');
-    await expect(logTimerPage.twentyfive).toBeVisible();
+  test('Set up the time with range slider', async () => {
+    await timerPage.slider.fill('25');
+    await expect(timerPage.minutes25).toBeVisible();
   });
 
-  test('Start the timer with the minimum time', async ({ page }) => {
-    await logTimerPage.startBtn.click();
-    await logTimerPage.getSessionElements();
-    await logTimerPage.expectSessionPage();
+  test('Start the timer with the minimum time', async () => {
+    await timerPage.startButton.click();
+    await timerPage.assertSessionPageLoaded();
+    await timerPage.assertSessionPageSuccessful();
   });
 
-  test('Start the timer with the maximum time', async ({ page }) => {
-    await logTimerPage.slider.fill('180');
-    await logTimerPage.startBtn.click();
-    await expect(logTimerPage.hundredeighty).toBeVisible();
-    await expect(logTimerPage.pandaStudying).toBeVisible();
-    await logTimerPage.expectSessionPage();
+  test('Start the timer with the maximum time', async () => {
+    await timerPage.slider.fill('180');
+    await timerPage.startButton.click();
+    await expect(timerPage.minutes180).toBeVisible();
+    await expect(timerPage.studyPanda).toBeVisible();
+    await timerPage.assertSessionPageSuccessful();
   });
 
   test('Timerpage stays on timer page after reload', async ({ page }) => {
     await page.reload();
-    await logTimerPage.expectTimerPage();
-    await logTimerPage.getTimerElements();
+    await timerPage.assertTimerPageLoaded();
   });
 });
